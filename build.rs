f4f001501cb644ea92e0b2de919a05f3605d11fd
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-24 04:12:01 +0000

    Compile CSS with minification enabled in release mode

--- build.rs
+++ build.rs
@@ -4,10 +4,16 @@ fn main() {
     println!("cargo:rerun-if-changed=./templates/*.html.tera");
     println!("cargo:rerun-if-changed=./styles/*");
 
+    let minify_arg = match cfg!(not(debug_assertions)) {
+        true => vec!["--minify"],
+        false => Vec::new()
+    };
+
     let output = Command::new("bash")
         .arg("./scripts/tailwind.sh")
         .args(["-i", "styles/styles.scss"])
         .args(["-o", "static/styles.css"])
+        .args(&minify_arg)
         .output()
         .expect("Failed to execute command");
 
